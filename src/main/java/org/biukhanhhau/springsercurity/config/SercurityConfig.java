3251b9a7a20b08b157ef7c8b5a8decbda32ee8f9
biukhanhhau <biu12.02dev@gmail.com>
2025-11-25 23:03:30 -0500

    more specific lambda expression in Config

--- src/main/java/org/biukhanhhau/springsercurity/config/SercurityConfig.java
+++ src/main/java/org/biukhanhhau/springsercurity/config/SercurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,10 +26,26 @@ public class SercurityConfig {
         chuyển sang trạng thái stateless cho session
         endregion*/
 
-        http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.csrf(customizer -> customizer.disable())
+//                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        Customizer<CsrfConfigurer<HttpSecurity>> disCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+                httpSecurityCsrfConfigurer.disable();
+            }
+        };
+        http.csrf(disCsrf);
+
+        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authentication = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+            }
+        };
+        http.authorizeHttpRequests(authentication);
 
         return http.build();
     }
